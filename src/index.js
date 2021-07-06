02bac8c34c7347e4a01c05c0c129169d0b6c13ed
Elisa Shapiro <elisashapiro@gmail.com>
2021-07-05 23:52:37 -0400

    first part done - saving here

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let submitVariable = document.getElementById("submitId")
+  submitVariable.addEventListener("click", (e) => {
+    e.preventDefault()
+    function newTask(text){
+      let newTaskItem = document.createElement("li")
+      newTaskItem.textContent = text
+      document.querySelector("#tasks").appendChild(newTaskItem)
+    }
+    let inputVariable = document.getElementById("new-task-description")
+    newTask(inputVariable.value)
+  })
+  
+
+
+
+
+
+
+
+
+
+
+
 });
